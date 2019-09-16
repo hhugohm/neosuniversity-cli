ae53de4010b66823ecd5ec0a4e9cd3968b6d7a45
hhugohm <hhugohm@gmail.com>
2019-09-16 10:51:55 -0500

    firts commit

---
+++ src/main/java/com/neos/university/neosuniversitycli/NeosuniversityCliApplication.java
@@ -0,0 +1,13 @@
+package com.neos.university.neosuniversitycli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeosuniversityCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeosuniversityCliApplication.class, args);
+	}
+
+}
